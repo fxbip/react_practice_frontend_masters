a18d3cf92fa29373f67637e0343112e81962dc1b
Ari <afriedgut1@gmail.com>
2019-09-08 15:47:15 +1000

    converted react elements to jsx, new plugins for eslint

--- src/App.js
+++ src/App.js
@@ -3,24 +3,32 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt me!"),
-    React.createElement(Pet, {
-      name: "Douggie",
-      animal: "Dog",
-      breed: "Maltese Shih-Tzu"
-    }),
-    React.createElement(Pet, {
-      name: "Gerald",
-      animal: "Dog",
-      breed: "German Shepherd"
-    }),
-    React.createElement(Pet, {
-      name: "Bryce",
-      animal: "Bird",
-      breed: "African Parrot"
-    })
-  ]);
+  // return React.createElement("div", {}, [
+  //   React.createElement("h1", {}, "Adopt me!"),
+  //   React.createElement(Pet, {
+  //     name: "Douggie",
+  //     animal: "Dog",
+  //     breed: "Maltese Shih-Tzu"
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Gerald",
+  //     animal: "Dog",
+  //     breed: "German Shepherd"
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Bryce",
+  //     animal: "Bird",
+  //     breed: "African Parrot"
+  //   })
+  // ]);
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <Pet name="Douggie" animal="Dog" breed="Maltese Shhih-Tzu" />
+      <Pet name="Spike" animal="Bird" breed="Parrot" />
+      <Pet name="Donkey Kong" animal="Gorilla" breed="Big Boy" />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
